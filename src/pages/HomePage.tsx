570c690ef19ea42b2445b426fb0f7ec78a87c959
Harish R <harish00743t@gmail.com>
2026-09-12 11:23:55 +0530

    Temporaly hide Experience section 12/09/2026
    Comment out the Experience component in HomePage.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
       <Content />
       <Explore />
       <Skills />
-      <Experience />
+      {/* <Experience /> */}
       <SocialLinks />
     </>
   );
